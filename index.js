57af759d865ec41825b05432998432806887a7f3
Parker Thompson <park.thompson@gmail.com>
2019-11-02 16:36:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  itemObject = new Object {
+    itemName: item,
+    itemPrice: math.random()
+  }
 }
 
 function viewCart() {
